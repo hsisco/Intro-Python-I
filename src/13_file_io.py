ea405a224f1b904b3b6b03c7c22f9ed4fe548373
hsisco <hysensisco@gmail.com>
2020-07-07 18:55:05 -0400

    file_io + bar.txt

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -4,16 +4,28 @@ at how to read and write to files here:
 
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
-
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
+with open("src/foo.txt") as foo:
+    foo.read()
+foo.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+with open("src/bar.txt","w") as bar:
+    bar.write('But if you do, sir, and for you, I serve as good a man as you!/n')
+    bar.write('No better?/n')
+    bar.write('Uh... uh.../n')
+    bar.write('Here comes our kinsman, say better!/n')
+    bar.write('Yes, sir, better!/n')
+    bar.write('You lie!/n')
+bar.close()
+
+with open("src/bar.txt") as bar:
+    bar.read()
+bar.close()
